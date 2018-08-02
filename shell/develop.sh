2054addbf5f46887227a9dcceda17a0523610e8a
Yuryi Rohalchuk <yrohaltc@training.local>
2018-08-02 19:52:18 +0300

    Test

--- shell/develop.sh
+++ shell/develop.sh
@@ -6,6 +6,12 @@
 #cd app directory
 
 #setup ours app + dependencies
+sudo yum -y update
+sudo yum -y install python
+sudo yum -y install mc
+sudo yum -y insall httpd
+sudo servce httpd start
+echo 'All done!!!'
 
-
+######
 exit 0
