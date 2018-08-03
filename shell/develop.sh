ccfa609718a253f0171d263ebbe098c71b3bc398
Alexsandr <1111-1111@ukr.net>
2018-08-03 17:37:06 +0300

    rename file READMY to READMY.md

--- shell/develop.sh
+++ shell/develop.sh
@@ -15,14 +15,14 @@ echo 'All done!!!'
 
 #install postgres
 yum -y install yum-utils postgresql-server postgresql-contrib
-ostgresql-setup initdb
+postgresql-setup initdb
 systemctl start postgresql
 systemctl enable postgresql
 #create database and user
 su - postgres -c  'psql -c "CREATE DATABASE test_db;"'
 sudo  -u postgres   psql -c "CREATE USER test_user WITH password '12345';"
 su - postgres -c  'psql -c "GRANT ALL privileges ON DATABASE test_db TO test_user;"'
-#modifed file /var/lib/pgsql/data/pg_hba.conf  for connetn to database use password
+#modifed file /var/lib/pgsql/data/pg_hba.conf  for connect to database use password
 sed  's/127.0.0.1\/32            ident/127.0.0.1\/32            md5/g'  /var/lib/pgsql/data/pg_hba.conf -i
 
 #install python3 pip end  virtualenv
