61a0d9f708f409d23132017dc652ad1f7473aece
Alexsandr <1111-1111@ukr.net>
2018-08-03 16:12:27 +0300

    Modified files develop.sh write rules for install postgres python3 pip virtualenv

--- shell/develop.sh
+++ shell/develop.sh
@@ -7,11 +7,32 @@
 
 #setup ours app + dependencies
 sudo yum -y update
-sudo yum -y install python
+sudo yum -y install python 
 sudo yum -y install mc
-sudo yum -y insall httpd
-sudo servce httpd start
+sudo yum -y intsall httpd
+sudo service httpd start
 echo 'All done!!!'
+
+#install postgres
+yum -y install yum-utils postgresql-server postgresql-contrib
+ostgresql-setup initdb
+systemctl start postgresql
+systemctl enable postgresql
+#create database and user
+su - postgres -c  'psql -c "CREATE DATABASE test_db;"'
+sudo  -u postgres   psql -c "CREATE USER test_user WITH password '12345';"
+su - postgres -c  'psql -c "GRANT ALL privileges ON DATABASE test_db TO test_user;"'
+#modifed file /var/lib/pgsql/data/pg_hba.conf  for connetn to database use password
+sed  's/127.0.0.1\/32            ident/127.0.0.1\/32            md5/g'  /var/lib/pgsql/data/pg_hba.conf -i
+
+#install python3 pip end  virtualenv
+yum -y groupinstall development
+yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+yum -y install python34 python-pip
+pip  install --upgrade pip
+pip install virtualenv
+
+
 
 ######
 exit 0
